397ebc3ecc36c69263cdf7eb073e058a4352ddd5
BabychOleksii <babych.oleksii@gmail.com>
2022-11-21 15:25:33 +0200

    Check products in the order

--- pythonSelenium/assignmentFunctional.py
+++ pythonSelenium/assignmentFunctional.py
@@ -6,6 +6,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
+expectedList = ['Cucumber - 1 Kg', 'Raspberry - 1/4 Kg', 'Strawberry - 1/4 Kg']
+actualList = []
+
 service_obj = Service('D:\\Projects\\Webdriver\\chromedriver.exe')
 driver = webdriver.Chrome(service=service_obj)
 driver.implicitly_wait(3)
@@ -14,12 +17,17 @@ driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.maximize_window()
 
 driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
-time.sleep(2)
+time.sleep(4)
+
 results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
 count = len(results)
 assert count > 0
 for result in results:
+    actualList.append(result.find_element(By.XPATH, "h4").text)
     result.find_element(By.XPATH, "div/button").click()
+
+# Check that we got correct products into the order
+assert expectedList == actualList
 
 driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
@@ -42,3 +50,7 @@ driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
 wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".promoInfo")))
 
 print(driver.find_element(By.CLASS_NAME, "promoInfo").text)
+
+# Check that discount is applied
+discountedAmount = float(driver.find_element(By.CSS_SELECTOR, ".discountAmt").text)
+assert totalAmount > discountedAmount
