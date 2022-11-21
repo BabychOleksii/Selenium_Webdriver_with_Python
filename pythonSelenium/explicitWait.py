70fa962293fd9aad0a4fa2b78ae162f00d2fbfe2
BabychOleksii <babych.oleksii@gmail.com>
2022-11-21 11:47:56 +0200

    Explicit wait

---
+++ pythonSelenium/explicitWait.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import time
+
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+service_obj = Service('D:\\Projects\\Webdriver\\chromedriver.exe')
+driver = webdriver.Chrome(service=service_obj)
+driver.implicitly_wait(3)
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+driver.maximize_window()
+
+driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
+time.sleep(2)
+results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
+count = len(results)
+assert count > 0
+for result in results:
+    result.find_element(By.XPATH, "div/button").click()
+
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "promoCode")))
+driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".promoInfo")))
+
+print(driver.find_element(By.CLASS_NAME, "promoInfo").text)
