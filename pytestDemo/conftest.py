d2aced55d1527a1b43538a291a5ddc2a1e390100
BabychOleksii <babych.oleksii@gmail.com>
2022-11-24 19:38:57 +0200

    Parametrizing test with fixture

--- pytestDemo/conftest.py
+++ pytestDemo/conftest.py
@@ -12,3 +12,9 @@ def setup():
 def data_load():
     print("User profile data is being created.")
     return ["Alex", "Babych", "Welcome on a board."]
+
+
+@pytest.fixture(params=[("chrome", "Alex", "Canada"), "firefox", "edge"])
+def cross_browser(request):
+    return request.param
+
--- pytestDemo/test_demo1.py
+++ pytestDemo/test_demo1.py
@@ -1,10 +1,16 @@
 import pytest
 
 
-def test_hello(setup):
-    print("Hello World!")
+# @pytest.mark.skip
+# def test_hello(setup):
+#     print("Hello World!")
+#
+#
+# def test_morning():
+#     print("Good Morning!")
 
 
-def test_morning():
-    print("Good Morning!")
+def test_cross_browser(cross_browser):
+    print(cross_browser)
+    print(cross_browser[2])
 
