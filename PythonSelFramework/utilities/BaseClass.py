f8f1034a805f356164c16c3aaac2773b24efe82d
BabychOleksii <babych.oleksii@gmail.com>
2022-12-13 16:18:17 +0200

    Creating custom utilities

--- PythonSelFramework/utilities/BaseClass.py
+++ PythonSelFramework/utilities/BaseClass.py
@@ -1,7 +1,13 @@
 import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 @pytest.mark.usefixtures("setup")
 class BaseClass:
-    pass
+
+    def verifyLinkPresence(self, text):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.presence_of_element_located((By.LINK_TEXT, text)))
 
