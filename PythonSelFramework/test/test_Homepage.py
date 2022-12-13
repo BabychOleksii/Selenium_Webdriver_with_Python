f07c24f40b3e61f4edf7a2d4de4b42f44ce46530
BabychOleksii <babych.oleksii@gmail.com>
2022-12-13 18:56:38 +0200

    Removing hard coding

--- PythonSelFramework/test/test_Homepage.py
+++ PythonSelFramework/test/test_Homepage.py
@@ -1,21 +1,26 @@
+import pytest
+
 from PythonSelFramework.pageObject.HomePage import HomePage
 from PythonSelFramework.utilities.BaseClass import BaseClass
 
 
 class TestHomePage(BaseClass):
 
-    def test_formSubmission(self):
+    def test_formSubmission(self, getData):
 
         homepage = HomePage(self.driver)
-        homepage.getName().send_keys("Alex")
-        homepage.getEmail().send_keys("alexbaum@mail.net")
+        homepage.getName().send_keys(getData[0])
+        homepage.getEmail().send_keys(getData[1])
         homepage.getCheckBox().click()
-        # sel = Select(homepage.getGender())
-        # sel.select_by_visible_text("Male")
-        self.selectOptionByText(homepage.getGender(), "Male")
+        self.selectOptionByText(homepage.getGender(), getData[2])
         homepage.submitForm().click()
 
         alertText = homepage.getSuccessMessage().text
 
         assert ("Success" in alertText)
 
+        self.driver.refresh()
+
+    @pytest.fixture(params=[("Alex", "alexbaum@mail.net", "Male"), ("Helen", "helen@gmail.com", "Female")])
+    def getData(self, request):
+        return request.param
