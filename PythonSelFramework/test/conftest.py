42f82c0c0fb129033ef3a97bef8a1c1ebcfc304a
BabychOleksii <babych.oleksii@gmail.com>
2022-12-14 20:59:15 +0200

    HTML reports with logging and screenshots

--- PythonSelFramework/test/conftest.py
+++ PythonSelFramework/test/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+driver = None
 
 from selenium.webdriver.chrome.service import Service
 
@@ -12,6 +13,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="class")
 def setup(request):
+    global driver
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
         driver = webdriver.Chrome(service=Service(executable_path="D:\\Projects\\Webdriver\\chromedriver.exe"))
@@ -25,4 +27,31 @@ def setup(request):
     request.cls.driver = driver
     yield
     driver.close()
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+        driver.get_screenshot_as_file(name)
 
